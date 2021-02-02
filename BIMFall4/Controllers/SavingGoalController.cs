dd9763d0ea43232e5006281fb9ee94eaaff82308
Emil <neanderemil@gmail.com>
2021-02-02 19:02:39 +0100

    savinggoalcontroller

---
+++ BIMFall4/Controllers/SavingGoalController.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace BIMFall4.Controllers
+{
+    public class SavingGoalController : ApiController
+    {
+        // GET: api/SavingGoal
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/SavingGoal/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/SavingGoal
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/SavingGoal/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/SavingGoal/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
