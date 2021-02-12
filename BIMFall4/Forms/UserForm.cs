1cd70cf6747afe6fa0640dc8844c38b36955078a
Salim98 <abas_salim@hotmail.com>
2021-02-12 10:09:30 +0000

    form for User.

---
+++ BIMFall4/Forms/UserForm.cs
@@ -0,0 +1,41 @@
+﻿using BIMFall4.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BIMFall4.Forms
+{
+    public class UserForm : User, IForm
+    {
+        private string _erroMessage = "";
+        public string ErrorMessage { get { return _erroMessage; } }
+
+        public bool isValid()
+        {
+            //if(!this.Email_isValid())
+            //{
+            //    this._erroMessage = "This email is already in use";
+            //    return false;
+            //}
+            //else if(!this.Password_isValid())
+            //{
+            //    this._erroMessage = "Password must be more than 8 characters";
+            //    return false;
+            //}
+            return true;
+        }
+
+        bool Email_isValid()
+        {
+            return true;
+        }
+
+        bool Password_isValid()
+        {
+            if (Password.Length < 8)
+                return false;
+            return true;
+        }
+    }
+}
