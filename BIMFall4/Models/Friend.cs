cfa45287c8f35afb85a03692b58e26bf71721552
Emil <neanderemil@gmail.com>
2021-03-09 16:44:47 +0100

    Social

--- BIMFall4/Models/Friend.cs
+++ BIMFall4/Models/Friend.cs
@@ -12,6 +12,11 @@ namespace BIMFall4.Models
 
         public int Id { get; set; }
         public int From_ID { get; set; }
+        public string From_Firstname { get; set; }
+        public string From_Lastname { get; set; }
+        [EmailAddress]
+        public string From_Email { get; set; }
+        [EmailAddress]
         public string To_Email { get; set; }
         public int To_ID { get; set; }
         public string Status { get; set; }
