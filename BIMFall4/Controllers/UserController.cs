4bef1d173e627523a8d4bb6d9df19676913ff15c
Martin <biszczak@live.se>
2021-03-16 14:38:46 +0100

    DTOusercontroller

--- BIMFall4/Controllers/UserController.cs
+++ BIMFall4/Controllers/UserController.cs
@@ -1,12 +1,16 @@
 ﻿using BIMFall4.Forms;
 using BIMFall4.Manager;
 using BIMFall4.Models;
+using BIMFall4.Data;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Threading.Tasks;
 using System.Web.Http;
+using System.Web.Http.Description;
+using System.Data.Entity;
 
 namespace BIMFall4.Controllers
 {
@@ -14,13 +18,29 @@ namespace BIMFall4.Controllers
     public class UserController : ApiController
     {
         // GET: api/User
-
-
-        // GET: api/User/5
-        public User GetUserById(int id)
+        public IQueryable<UserDto> GetUser(int id)
         {
-            return UserManager.GetUserByID(id);
+            using (var db = new BIMFall4Context())
+            {
+                var user = from x in db.Users
+                           select new UserDto()
+                           {
+                               UserID = x.UserID,
+                               FirstName = x.FirstName,
+                               LastName = x.LastName,
+                               Email = x.Email
+                           };
+                return user;
+            }
         }
+
+        //googla - - - eagerload relations
+
+        //GET: api/User/5
+        //public User GetUserById(int id)
+        //{
+        //    return UserManager.GetUserByID(id);
+        //}
 
         // POST: api/User
         public Response Post([FromBody] User value)
--- BIMFall4/Models/User.cs
+++ BIMFall4/Models/User.cs
@@ -7,24 +7,28 @@ using System.Web;
 
 namespace BIMFall4.Models
 {
+    public class UserDto
+    {
+        public int UserID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+    }
+
+
     public class User
     {
         [Key]
         public int UserID { get; set; }
-
         [Required(AllowEmptyStrings = false), MaxLength(80)]
         public string FirstName { get; set; }
-
         [Required(AllowEmptyStrings = false), MaxLength(80)]
         public string LastName { get; set; }
-
         [Index(IsUnique = true), MaxLength(80)]
         [EmailAddress]
         public string Email { get; set; }
-
         [Required(AllowEmptyStrings = false), MinLength(8)]
         public string Password { get; set; }
-
         public virtual ICollection<Income> Incomes { get; set; }
         public virtual ICollection<Expense> Expenses { get; set; }
         public virtual ICollection<Budget> Budgets { get; set; }
