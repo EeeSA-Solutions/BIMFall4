88a04bed54067bad79d6f7e7589d1e90e4c674d5
Erik <boneblooder@hotmail.com>
2021-10-25 10:36:54 +0200

    Byggt vidare på progress. Dock så strular det lite

---
+++ BIMFall4/Controllers/CalculateController.cs
@@ -0,0 +1,42 @@
+﻿using BIMFall4.Manager.CalculateManagers;
+using BIMFall4.ModelDTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace BIMFall4.Controllers
+{
+    public class CalculateController : ApiController
+    {
+        // GET: api/Calculate
+        public List<ProgressCalculations.Erik> Get()
+        {
+            var johan = ProgressCalculations.Calculate(1);
+            return johan;
+        }
+
+        // GET: api/Calculate/5
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/Calculate
+        public void Post([FromBody]string value)
+        {
+        }
+
+        // PUT: api/Calculate/5
+        public void Put(int id, [FromBody]string value)
+        {
+        }
+
+        // DELETE: api/Calculate/5
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ BIMFall4/Manager/CalculateManagers/ProgressCalculations.cs
@@ -0,0 +1,47 @@
+﻿using BIMFall4.ModelDTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BIMFall4.Manager.CalculateManagers
+{
+
+    public class ProgressCalculations
+    {
+        ///  <summary> 
+        ///        Hämta budget per enskild kategori och deras expense.
+        ///         - Lägg sedan ihop dessa expenses och skicka den summan och budget summan till front(1200/5000)
+        ///         Gör sedan en total uträkning för alla expenses och alla budgetar och skicka till front (13000/17000)
+        ///         Sortera efter månad.
+        ///  </summary>
+       public struct Erik
+        {
+            string Category;
+            int Count;
+            decimal Amount;
+            
+            public Erik(string category, int count, decimal amount)
+            {
+                Category = category;
+                Count = count;
+                Amount = amount;
+            }
+
+
+        }
+        public static List<Erik> Calculate(int userId)
+        {
+            var list = ExpenseManager.GetUserExpenseDtoSortedByCategoryAndCurrentMonth(userId);
+            var sortedlist=list.GroupBy(item=>item.Category)
+            .Select(item => new  Erik
+                           (
+                               item.Key,
+                               item.Count(),
+                               item.Sum(ta => ta.Amount)
+                           )).ToList();
+
+            return sortedlist;
+        } 
+
+    }
+}
