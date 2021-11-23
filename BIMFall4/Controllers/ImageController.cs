2c4052f8f01f71f70fa5806fc6bade80379d0681
tobiasp89tp@gmai.com <tobiasp89tp@gmail.com>
2021-11-23 16:45:42 +0100

    IMageController

---
+++ BIMFall4/Controllers/ImageController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BIMFall4.Controllers
+{
+    [System.Web.Http.Cors.EnableCors(origins: "*", headers: "*", methods: "*")]
+    public class ImageController : Controller
+    {
+        // GET: Image
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
